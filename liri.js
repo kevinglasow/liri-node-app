5c33360b42406bc7fb0072d1ae89c810fbb9a12d
Kevin J Glasow <kevin.glasow@gmail.com>
2018-04-04 13:26:45 -0400

    deleting modules

--- liri.js
+++ liri.js
@@ -4,3 +4,12 @@ var input = process.argv.slice(2);
 
 console.log(input);
 
+// Spotify client ID: 055e4f2da30d44a7942b16eea766e597
+// Spotify client secret: 35de7a10582a4a34b169a3684cc0a746
+
+// Twitter consumer key: UdbcJbdXtNiO5zscvh96LmEGS 
+// Twitter consumer secret: iqgTVkklDUXAlfCg4CNagZb8fdjApPmEkuwSK299E8a9OVwO3x
+// Twitter access token: 148950079-7H0joTdInWXsbnjEW2Q42eNkQf3zDb91GjO6GKdY
+// Twitter access secret: y6JhBjObBHALKFWxNRJ7CbKt8Z0ASPi3DlIoEKEboF1hh
+
+
