86289764da808015db9b1b92bb5b0d2ea8786506
Kevin J Glasow <kevin.glasow@gmail.com>
2018-04-05 18:01:57 -0400

    pushing without node modules

--- liri.js
+++ liri.js
@@ -5,21 +5,30 @@ var Spotify = require("node-spotify-api");
 var keys = require("./keys.js")
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+var fs = require("fs");
 
 var input = process.argv.slice(2);
-console.log(input);
+var command = input[0];
+var userEntry = input[1];
 
-if (input[0] === "my-tweets") {
+console.log(command);
+console.log(userEntry);
+
+if (command === "my-tweets") {
     myTweets()
-} else if (input[0] === "spotify-this-song") {
+} else if (command === "spotify-this-song") {
     if (input.length === 1) {
-        spotifyThisSong("Let It Be")
+        spotifyThisSong("The Sign")
     } else {
-        spotifyThisSong(input[1])
+        spotifyThisSong(command)
     }
-} else if (input[0] === "movie-this") {
-    movieThis()
-} else if (input[0] === "do-what-it-says") {
+} else if (command === "movie-this") {
+    if (input.length === 1) {
+        movieThis("Mr. Nobody")
+    } else {
+        movieThis(userEntry)
+    }
+} else if (command === "do-what-it-says") {
     doWhatItSays()
 } else {
     Console.log("Please enter a command")
@@ -31,13 +40,14 @@ function myTweets() {
         count: "20"
     };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (!error) {
-            tweets.forEach(function (tweet) {
-                console.log(tweet.text);
-                console.log(tweet.created_at);
-                console.log("");
-            })
+        if (error) {
+            return console.log("Error occurred: " + error);
         }
+        tweets.forEach(function (tweet) {
+            console.log(tweet.text);
+            console.log(tweet.created_at);
+            console.log("");
+        })
     });
 }
 
@@ -46,17 +56,46 @@ function spotifyThisSong(track) {
         type: 'track',
         query: track,
         limit: 1
-    }, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
+    }, function (error, data) {
+        if (error) {
+            return console.log("Error occurred: " + error);
         }
         // data.forEach(function (songs) {
-            // console.log(JSON.stringify(data.tracks.items[0], null, 2));
-            console.log("Artist Name: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
-            console.log("Song Title: " + JSON.stringify(data.tracks.items[0].name, null, 2));
-            console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
-            console.log("Album Name: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
-            console.log("");
+        // console.log(JSON.stringify(data.tracks.items[0], null, 2));
+        console.log("Artist Name: " + JSON.stringify(data.tracks.items[0].artists[0].name, null, 2));
+        console.log("Song Title: " + JSON.stringify(data.tracks.items[0].name, null, 2));
+        console.log("Preview URL: " + JSON.stringify(data.tracks.items[0].preview_url, null, 2));
+        console.log("Album Name: " + JSON.stringify(data.tracks.items[0].album.name, null, 2));
+        console.log("");
         // })
+    });
+}
+
+function movieThis(title) {
+    request("http://www.omdbapi.com/?apikey=trilogy&t=" + title, function (error, response, body) {
+        if (error) {
+            return console.log("Error occured: " + error);
+        }
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+        console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Production Country: " + JSON.parse(body).Country);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot Summary: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+    });
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+        if (error) {
+            return console.log(error);
+        }
+        var dataArr = data.split(",");
+        var command = dataArr[0];
+        var userEntry = dataArr[1]
+        console.log(command);
+        console.log(userEntry);
     });
 }
